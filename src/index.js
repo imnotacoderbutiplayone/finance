e4e00cdd22be0285c7c448aa7a503da5b54674a9
imnotacoderbutiplayone <leaves-jog.0b@icloud.com>
2025-07-18 10:32:40 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PortfolioStressTester from './PortfolioStressTester';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PortfolioStressTester />);
